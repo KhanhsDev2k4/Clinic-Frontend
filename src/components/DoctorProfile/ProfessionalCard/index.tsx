4fcd8c631a2a8e2a06433987d59cf5ee565c9924
2iamQuan <quannvm@vnpt.vn>
2026-05-18 14:06:10 +0700

    fix: uncomment import statement for useEffect and useState in ProfessionalCard component

--- src/components/DoctorProfile/ProfessionalCard/index.tsx
+++ src/components/DoctorProfile/ProfessionalCard/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import NumberFlow from "@number-flow/react";
 import { useCurrentProfile } from "@/hooks/auth/useCurrentProfile";
 
